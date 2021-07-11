5993b2fc8bd984f3df36ed3ff7cf62c0da1fb540
zearkiatos <caprilespe@outlook.com>
2021-07-11 14:29:58 -0400

    Initialization project

---
+++ Csharp.UsersApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Csharp.UsersApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Csharp.UsersApi/Configuration/Constants.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Csharp.UsersApi.Configuration
+{
+    public static class Constants
+    {
+
+        public static string ENVIRONMENT = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    }
+}
